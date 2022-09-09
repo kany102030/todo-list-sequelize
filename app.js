5943335c354dd218ba23a788b82735b8eb50e32c
kany <kany102030@gmail.com>
2022-09-09 22:49:23 +0800

    feat: add login function

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const bcrypt = require('bcryptjs')
+const usePassport = require('./config/passport')
+const session = require('express-session')
 const app = express()
 const PORT = 3000
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
 app.use(routes)
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,15 +2,17 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models')
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 const User = db.User
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  res.send('login')
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
