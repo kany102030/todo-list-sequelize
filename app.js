b1b27d76e795924b71cc28aeee484d6f146a8c13
kany <kany102030@gmail.com>
2022-09-09 15:48:17 +0800

    Project init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const routes = require('./routes')
+const bcrypt = require('bcryptjs')
+const app = express()
+const PORT = 3000
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(routes)
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
